d8b477e438c747221f7b2a06c7ce3d856290edfb
Alex Uemura <alex.yukio.uemura@gmail.com>
2022-08-02 21:31:57 -0300

    first commit

---
+++ src/main/java/org/example/GreetingService.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+
+}
